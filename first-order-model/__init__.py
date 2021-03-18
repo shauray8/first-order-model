c64aacf84e16f606a0442b296d55942cc18d8e2a
Shauray <shauray9@gmail.com>
2021-03-18 12:38:47 +0530

    architecture

---
+++ first-order-model/__init__.py
@@ -0,0 +1,7 @@
+from model import *
+from train import *
+
+"""
+reading the research paper for first order model 
+-- 
+"""
---
+++ first-order-model/model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class model(nn.Module):
+    def __init__(self):
+        super(model, self).__init__()
+        pass
+
+    def forward(self,x):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+
+
